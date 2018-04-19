7fca86233f331f53e5ec9a3307e8266b91e24021
Flo <florianjunker96@gmail.com>
2018-04-19 17:12:13 +0200

    Update firstbot.php

--- firstbot.php
+++ firstbot.php
@@ -1,11 +1,8 @@
-<html>
 <?php
+  $botToken = "550038506:AAEJhUC9t-j-SPPyDZ_g0zQofXT2n9ln0UU";
+  $website = "https://api.telegram.org/bot".$botToken;
 
-$botToken = "550038506:AAEJhUC9t-j-SPPyDZ_g0zQofXT2n9ln0UU";
-$website = "https://api.telegram.org/bot".$botToken;
+  $update = file_get_contents($webite."/getupdates");
 
-$update = file_get_contents($webite."/getupdates");
-
-print_r($update);
+  print_r($update);
 ?>
-</html>
